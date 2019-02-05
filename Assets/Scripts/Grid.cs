0be4901a73d0ed32488f0333a478dba0bc93b01a
Unknown <coreyd2108@gmail.com>
2019-02-05 14:18:22 -0500

    Add generic grid for object placement

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    [SerializeField]
+    public int x_size = 5;
+    [SerializeField]
+    public int y_size = 5;
+
+    // Initialize grip
+    private void Start()
+    {
+        DrawGrid();
+    }
+
+    private void DrawGrid()
+    {
+        // Create gird squares for visualization
+        for (int i = 0; i < x_size; i++)
+        {
+            for (int j = 0; j < y_size; j++)
+            {
+                GameObject square = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                square.transform.position = new Vector3(i, 0, j);
+                square.transform.localScale = new Vector3(0.075f, 0.075f, 0.075f);
+                square.GetComponent<Renderer>().material.color = Color.green;
+                square.AddComponent<GridSquare>();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GridSquare.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSquare : MonoBehaviour {
+
+    private void OnMouseExit()
+    {
+        this.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    private void OnMouseOver()
+    {
+        this.GetComponent<Renderer>().material.color = Color.red;
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = this.transform.position;
+        }
+    }
+}
