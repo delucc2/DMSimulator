4e502371d67a130c5c6fd9de62fe7ffdd2d92b58
Unknown <coreyd2108@gmail.com>
2019-02-05 16:26:02 -0500

    Grid adds prefabs instead of cubes

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,19 +9,54 @@ public class Grid : MonoBehaviour {
     [SerializeField]
     public int y_size = 5;
 
-    // Initialize grip
+    public Transform wall;
+
+    /* Selection Key
+     * --------------
+     * 0 - Nothhing
+     * 1 - Wall
+     * 2 - Pit
+     * 3 - Crushing Wall
+     * 4 - Spikes
+     * 5 - Boulder
+     * 6 - Arrow Wall
+     * 9 - Delete
+     */
+    private char selection = '0';
+
+    // Initialize grid
     private void Start()
     {
         DrawGrid();
     }
 
+    // Reads for hotkeys to switch selection
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0)) {
+            selection = '0';
+        } else if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            selection = '1';
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            selection = '2';
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            selection = '3';
+        } else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            selection = '4';
+        } else if (Input.GetKeyDown(KeyCode.Alpha5)) {
+            selection = '5';
+        } else if (Input.GetKeyDown(KeyCode.Alpha6)) {
+            selection = '6';
+        } else if (Input.GetKeyDown(KeyCode.Alpha9)) {
+            selection = '9';
+        }
+    }
+
     private void DrawGrid()
     {
-        // Create gird squares for visualization
-        for (int i = 0; i < x_size; i++)
-        {
-            for (int j = 0; j < y_size; j++)
-            {
+        // Create grid squares for visualization
+        for (int i = 0; i < x_size; i++) {
+            for (int j = 0; j < y_size; j++) {
                 GameObject square = GameObject.CreatePrimitive(PrimitiveType.Plane);
                 square.transform.position = new Vector3(i, 0, j);
                 square.transform.localScale = new Vector3(0.075f, 0.075f, 0.075f);
@@ -29,5 +64,10 @@ public class Grid : MonoBehaviour {
                 square.AddComponent<GridSquare>();
             }
         }
+    }
+
+    public int GetSelection()
+    {
+        return selection;
     }
 }
--- Assets/Scripts/GridSquare.cs
+++ Assets/Scripts/GridSquare.cs
@@ -3,19 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridSquare : MonoBehaviour {
+    private Grid grid;
+
+    public void Start()
+    {
+        grid = GameObject.Find("Grid").GetComponent<Grid>();
+    }
 
     private void OnMouseExit()
     {
         this.GetComponent<Renderer>().material.color = Color.green;
     }
 
+    // Highlights grid square, and allows placement of object
     private void OnMouseOver()
     {
         this.GetComponent<Renderer>().material.color = Color.red;
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.position = this.transform.position;
+            switch (grid.GetSelection()) {
+                case '1' :
+                    Transform wall = Instantiate(grid.wall, this.transform);
+                    wall.transform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
+                    break;
+                case '2' :
+                    break;
+                case '3':
+                    break;
+                case '4':
+                    break;
+                case '5':
+                    break;
+                case '6':
+                    break;
+                case '9':
+                    break;
+            }
         }
     }
 }
