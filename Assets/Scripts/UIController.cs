76443c5d4a8f1d59b00b4a9e19f692fcd33817f7
Unknown <coreyd2108@gmail.com>
2019-04-23 05:48:37 -0400

    Add "Are You Sure?" Dialogue box when returning to main menu

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class UIController : MonoBehaviour {
 
@@ -19,5 +20,29 @@ public class UIController : MonoBehaviour {
     public void goToMenu()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    public void AreYouSure(string option)
+    {
+        Time.timeScale = 0;
+        if (option == "main menu") {
+            GameObject.Find("Are You Sure?").transform.GetChild(0).GetChild(1).GetComponent<UnityEngine.UI.Text>().text = "Are you sure you want to quit?";
+            Show(GameObject.Find("Are You Sure?"));
+            Show(GameObject.Find("Quit"));
+        } else if (option == "restart") {
+            GameObject.Find("Are You Sure?").transform.GetChild(0).GetChild(1).GetComponent<UnityEngine.UI.Text>().text = "Are you sure you want to restart?";
+            Show(GameObject.Find("Are You Sure?"));
+            Show(GameObject.Find("Restart"));
+        }
+    }
+
+    public void Restart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
     }
 }
